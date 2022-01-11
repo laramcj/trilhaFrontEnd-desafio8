4ce5a53c289f58eb275e2d65ae803eb35bba447c
Lara Correa <lara.mcj@gmail.com>
2022-01-11 16:52:00 -0300

    adding in-memory-database for categories

---
+++ finansys/src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+import { Observable } from "rxjs";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(): {
+        const categories: Category[] = [
+            {id: 1, name: 'Moradia', description: 'Pagamento de Contas da Casa'},
+            {id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
+            {id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
+            {id: 4, name: 'Salário', description: 'Recebimento de Salário'},
+            {id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'},
+        ];
+
+        return{ categories }
+
+    } 
+}
