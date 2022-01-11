359601e9d4551184784c75f09b2b328e1d680280
Lara Correa <lara.mcj@gmail.com>
2022-01-11 18:04:05 -0300

    adding category service

--- finansys/src/app/in-memory-database.ts
+++ finansys/src/app/in-memory-database.ts
@@ -3,7 +3,7 @@ import { Category } from "./pages/categories/shared/category.model";
 
 export class InMemoryDatabase implements InMemoryDbService{
     createDb(): {
-        const categories: Category[] = [
+        categories: Category[] = [
             {id: 1, name: 'Moradia', description: 'Pagamento de Contas da Casa'},
             {id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
             {id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
