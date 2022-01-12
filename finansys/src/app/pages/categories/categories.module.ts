00a51ae0f203e8c8e613ef0a1110faea2e883542
Lara Correa <lara.mcj@gmail.com>
2022-01-12 17:01:52 -0300

    improving CategoryFormComponent

--- finansys/src/app/pages/categories/categories.module.ts
+++ finansys/src/app/pages/categories/categories.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
-  imports: [CommonModule, CategoriesRoutingModule],
+  imports: [CommonModule, CategoriesRoutingModule, ReactiveFormsModule],
 })
 export class CategoriesModule {}
